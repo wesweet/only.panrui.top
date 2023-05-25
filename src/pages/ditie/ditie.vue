68f074c83b1176122b6cd8773ac5a6bdafe7be80
panr99 <1547177202@qq.com>
2023-05-25 17:13:18 +0800

    新增地铁展示页面

---
+++ src/pages/ditie/ditie.vue
@@ -0,0 +1,11 @@
+<template>
+	<uni-nav-bar shadow dark title="地铁线路图" />
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/treasure/treasure.vue
@@ -0,0 +1,90 @@
+<template>
+	<uni-nav-bar shadow dark title="百宝箱" />
+	<uni-grid :column="3" :square="false" :highlight="false" @change="change">
+		<uni-grid-item v-for="(item, index) in list" :index="index" :key="index">
+			<view class="grid-item-box">
+				<image :src="item.url" class="image" mode="aspectFill" />
+				<text class="text">{{ item.text }}</text>
+			</view>
+		</uni-grid-item>
+	</uni-grid>
+</template>
+
+<script lang="ts" setup>
+	import { reactive } from 'vue';
+	const list = reactive([
+		{
+			text: "地铁线路图",
+			url: "/src/static/ditie.png",
+			page: "/pages/ditie/ditie"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		},
+		{
+			text: "地铁线路图",
+			url: "/src/static/mieba.png"
+		}
+	])
+	const change = (e : any) => {
+		const page = list[e.detail.index].page
+		if (!page) {
+			uni.showToast({
+				title: '功能正在开发中',
+				duration: 2000
+			});
+		}
+		uni.navigateTo({
+			url: page
+		})
+	}
+</script>
+
+<style lang="scss">
+	.grid-item-box {
+		flex: 1;
+		// position: relative;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 15px 0;
+	}
+
+	.image {
+		width: 25px;
+		height: 25px;
+	}
+
+	.text {
+		font-size: 14px;
+		margin-top: 5px;
+	}
+</style>
