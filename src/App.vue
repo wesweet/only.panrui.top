f6d1f0eff786b801e016a9dd2cbd5a2a9c357035
panr99 <1547177202@qq.com>
2023-05-31 16:00:19 +0800

    1.0 首页添加轮播滚动消息

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-	import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
-	onLaunch(() => {
-		console.log("App Launch");
-		const app = getApp({ allowDefault: true })
-		app.globalData = {
-			geoUrl: 'https://restapi.amap.com/v3', // 高德地图接口服务路径
-			key: 'eef520430048905703a89eef94f73d23',// 高德地图web_key
-			metrokey: '2676091421668dc3a8b7534e5cc3b502' //高德地铁 key
-		}
-	});
-	onShow(() => {
-		// console.log("App Show");
-	});
-	onHide(() => {
-		// console.log("App Hide");
-	});
+import { onLaunch, onShow, onHide } from "@dcloudio/uni-app";
+onLaunch(() => {
+//   console.log("App Launch");
+  const app = getApp({ allowDefault: true });
+  app.globalData = {
+    geoUrl: "https://restapi.amap.com/v3", // 高德地图接口服务路径
+    key: "eef520430048905703a89eef94f73d23", // 高德地图web_key
+    metrokey: "2676091421668dc3a8b7534e5cc3b502", //高德地铁 key
+  };
+});
+onShow(() => {
+  // console.log("App Show");
+  // 判断本地缓存是否存在token: 不存在则跳转到登录页
+  if (!uni.getStorageSync("token")) {
+    uni.redirectTo({ url: "/pages/login/login" });
+  }
+});
+onHide(() => {
+  // console.log("App Hide");
+});
 </script>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,24 @@
+<!--
+ * @Description: 首页
+ * @Author: panrui
+ * @Date: 2023-05-25 14:12:37
+ * @LastEditTime: 2023-05-31 15:58:49
+ * @LastEditors: panrui
+ * 不忘初心,不负梦想
+-->
 <template>
   <StatusBar></StatusBar>
-  <uni-nav-bar shadow dark title="首页" />
+  <uni-notice-bar
+    background-color="#e60033"
+    color="#ffffff"
+    show-icon
+    scrollable
+    text="1208App正式发布了、欢迎大家下载体验、有问题请联系我(1547177202)、谢谢！"
+  />
 </template>
 
 <script setup lang="ts">
 import StatusBar from "@/components/StatusBar.vue";
-// 判断本地缓存是否存在token: 不存在则跳转到登录页
-if (!uni.getStorageSync("token")) {
-  uni.redirectTo({ url: "/pages/login/login" });
-}
 </script>
 
 <style lang="scss" scoped></style>
