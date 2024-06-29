f9b5a68fb2db4f5577277678ef022102bf25f46f
panrui <1547177202@qq.com>
2024-06-29 18:47:48 +0800

    修改端口

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,11 +2,11 @@
  * @Description:
  * @Author: panrui
  * @Date: 2023-05-25 15:26:42
- * @LastEditTime: 2024-04-16 20:00:56
- * @LastEditors: prui
+ * @LastEditTime: 2024-06-29 18:47:17
+ * @LastEditors: panrui 1547177202@qq.com
  * 不忘初心,不负梦想
  */
 // export const HTTP_REQUEST_URL = `http://nest.panrui.top:8008/app`; // 线上环境
-export const HTTP_REQUEST_URL = `https://work.panrui.top:12081/h5`; // 线上环境
+export const HTTP_REQUEST_URL = `https://work.panrui.top:8443/h5`; // 线上环境
 // export const HTTP_REQUEST_URL = `http://192.168.31.83:9008/app`; // 开发环境
 // export const HTTP_REQUEST_URL = `http://localhost:9008/h5`; // 开发环境
