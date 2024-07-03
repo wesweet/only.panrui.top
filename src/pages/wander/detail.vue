2e849bc36abf2d343da74aaf8b6b7168e76c6c21
panrui <1547177202@qq.com>
2024-07-03 23:48:13 +0800

    逻辑调整

--- src/pages/wander/detail.vue
+++ src/pages/wander/detail.vue
@@ -22,15 +22,32 @@
           </view>
         </up-form-item>
         <up-form-item label="图片" prop="photo" borderBottom>
-          <up-upload :fileList="imageList" name="1" :maxCount="1" width="300" height="300" @delete="deleteImage"></up-upload>
+          <up-upload
+            :fileList="imageList"
+            name="1"
+            :maxCount="1"
+            width="300"
+            height="300"
+            @afterRead="afterRead"
+            @delete="deleteImage"
+          ></up-upload>
         </up-form-item>
         <up-form-item label="内容" prop="content" borderBottom>
-          <up-textarea v-model="baseFormData.content" placeholder="请输入内容" ></up-textarea>
+          <up-textarea
+            v-model="baseFormData.content"
+            placeholder="请输入内容"
+          ></up-textarea>
         </up-form-item>
       </up-form>
 
       <view class="btn-box">
-        <up-button class="primary-btn" text="提交"></up-button>
+        <up-button
+          :loading="loading"
+          loadingText="加载中"
+          class="primary-btn"
+          text="提交"
+          @click="onSubmit"
+        ></up-button>
       </view>
     </view>
     <up-calendar
@@ -48,45 +65,6 @@ import { request } from "@/utils/request";
 import { onLoad, onShow } from "@dcloudio/uni-app";
 import { WANDER_API } from "@/api/wander";
 
-const show = ref(false);
-const mode = ref("single");
-const confirm = (e: any) => {
-  baseFormData.date = e[0];
-  show.value = false;
-};
-// 使用ts 定义表单数据类型接口
-interface BaseFormData {
-  title: string;
-  date: string;
-  content: string;
-}
-// 定义表单数据
-const baseFormData: BaseFormData = reactive({
-  title: "",
-  date: "",
-  content: "",
-});
-
-// 定义表单验证规则
-const rules = reactive({
-  title: {
-    rules: [{ required: true, errorMessage: "请输入标题" }],
-  },
-  date: {
-    rules: [{ required: true, errorMessage: "请选择日期" }],
-  },
-  content: {
-    rules: [{ required: true, errorMessage: "请输入内容" }],
-  },
-});
-
-// 定义按钮状态
-const loading = ref(false);
-
-// 定义图片地址
-
-// 定义一个名为 maskClick 的箭头函数
-const maskClick = () => {};
 const id = ref("");
 // 判断上一个页面传递过来的id
 onLoad((options: any) => {
@@ -108,18 +86,23 @@ const getWanderDetail = () => {
   })
     .then((res: any) => {
       const { errorCode, errorMessage, data } = res;
-      if (errorCode == 0) {
-        if (data) {
-          baseFormData.title = data.title;
-          baseFormData.date = data.date;
-          baseFormData.content = data.content;
-          if (data.photo) {
-            imageList.push({
-              name: "image",
-              url: data.photo,
-            });
-          }
-        }
+      if (errorCode != 0 || !data) {
+        uni.showToast({
+          title: errorMessage,
+          duration: 2000,
+          icon: "error",
+        });
+        return;
+      }
+
+      baseFormData.title = data.title;
+      baseFormData.date = data.date;
+      baseFormData.content = data.content;
+      if (data.photo) {
+        imageList.push({
+          name: "image",
+          url: data.photo,
+        });
       }
     })
     .catch((err: any) => {
@@ -127,42 +110,44 @@ const getWanderDetail = () => {
     });
 };
 
-const imageList: Array<{ url: string; name: string }> = reactive([]);
-
-// const uploadFile = () => {
-//   uni.chooseImage({
-//     count: 1,
-//     success(res: any) {
-//       for (let i = 0; i < res.tempFilePaths.length; i++) {
-//         const uniqueSuffix = `${Date.now()}${Math.round(Math.random() * 1e9)}`;
-//         imageList.push({
-//           name: uniqueSuffix,
-//           url: res.tempFilePaths[i],
-//         });
-//       }
-//     },
-//   });
-// };
-// 删除图片
-const deleteImage = (event: { index: number; }) => {
-  imageList.splice(event.index, 1);
-};
-
-
+// 定义按钮状态
+const loading = ref(false);
+// 定义表单验证规则
+const rules = reactive({
+  title: {
+    type: "string",
+    required: true,
+    message: "标题不能为空",
+    trigger: ["blur", "change"],
+  },
+  date: {
+    type: "string",
+    required: true,
+    message: "日期不能为空",
+    trigger: ["blur", "change"],
+  },
+  content: {
+    type: "string",
+    required: true,
+    message: "内容不能为空",
+    trigger: ["blur", "change"],
+  },
+});
 const valiForm = ref<any>(null);
 
 const onSubmit = () => {
   valiForm.value.validate().then((res: any) => {
-    if (id.value) {
-      res.id = id.value;
+    if (!res) {
+      return;
     }
     loading.value = true;
     uni.uploadFile({
       url: WANDER_API.addWander,
       files: imageList,
-      formData: res,
+      formData: Object.assign({},baseFormData, {
+        id: id.value,
+      }),
       success(result: any) {
-        loading.value = false;
         const data = JSON.parse(result.data);
         const { errorCode, message } = data;
         if (errorCode == 0) {
@@ -170,15 +155,49 @@ const onSubmit = () => {
             title: message,
             icon: "success",
             duration: 500,
-            success() {},
           });
         }
       },
       fail(err: any) {
+        console.log(err);
+      },
+      complete() {
         loading.value = false;
       },
     });
   });
+};
+
+const show = ref(false);
+const mode = ref("single");
+const confirm = (e: any) => {
+  baseFormData.date = e[0];
+  show.value = false;
+};
+// 使用ts 定义表单数据类型接口
+interface BaseFormData {
+  title: string;
+  date: string;
+  content: string;
+}
+// 定义表单数据
+const baseFormData: BaseFormData = reactive({
+  title: "",
+  date: "",
+  content: "",
+});
+
+const imageList: Array<{ url: string; name: string }> = reactive([]);
+
+const afterRead = (event: any) => {
+  imageList.push({
+    name: event.file.name,
+    url: event.file.url,
+  });
+};
+// 删除图片
+const deleteImage = (event: { index: number }) => {
+  imageList.splice(event.index, 1);
 };
 </script>
 
